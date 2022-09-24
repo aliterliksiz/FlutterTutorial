edf3d8fc593fa7831258e97a03a46c37cf31dd41
Ali Terliksiz <aliterliksiz46@gmail.com>
2022-09-24 16:02:33 +0300

    AppBar added Lesson 3 part 2

---
+++ lib/101/app_bar_learn.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppBarLearnView extends StatelessWidget {
+  const AppBarLearnView({Key? key}) : super(key: key);
+  final String _title = 'Welcome to Learn';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(_title),
+        centerTitle: true,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        leading: Icon(Icons.chevron_left),
+        leadingWidth: 23,
+        actionsIconTheme: IconThemeData(color: Colors.red, size: 40),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz)),
+          Center(
+            child: CircularProgressIndicator(),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/101/app_bar_learn.dart';
 import 'package:flutter_application_1/101/button_learn.dart';
 import 'package:flutter_application_1/101/container_sized_box_learn.dart';
 import 'package:flutter_application_1/101/scaffold_learn.dart';
@@ -17,8 +19,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: ButtonLearn(),
+      theme: ThemeData.dark().copyWith(
+          appBarTheme: const AppBarTheme(
+              centerTitle: true,
+              systemOverlayStyle: SystemUiOverlayStyle.light,
+              elevation: 0,
+              backgroundColor: Colors.red)),
+      //Tema ile uğraşarak belirli bir standarta sahip olmalıyız.
+      //Her sayfada appbar olmaz
+      //Sayfaların standartlarını main.dart'ta yazacağız sonra bir daha yazmayacağız
+      home: AppBarLearnView(),
     );
   }
 }
