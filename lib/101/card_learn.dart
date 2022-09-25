9ed23b306f10f538260b083184a95d5cada74740
Ali Terliksiz <aliterliksiz46@gmail.com>
2022-09-25 16:44:56 +0300

    Lesson 4 Part 3: Card component added

--- lib/101/card_learn.dart
+++ lib/101/card_learn.dart
@@ -10,10 +10,23 @@ class CardLearn extends StatelessWidget {
       body: Column(
         children: [
           Card(
+            color: Colors
+                .white, //Color'ı bu şekilde vermek yanlıştır aşağidaki gibi theme'dan vereceğiz
+            margin: ProjectMargin.cardMarging,
             elevation: 100,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
             child: SizedBox(
               height: 100,
-              width: 100,
+              width: 300,
+              child: Center(child: Text('Ali')),
+            ),
+          ),
+          _CustomCards(
+            child: SizedBox(
+              height: 100,
+              width: 300,
+              child: Center(child: Text('Ali')),
             ),
           )
         ],
@@ -21,3 +34,29 @@ class CardLearn extends StatelessWidget {
     );
   }
 }
+
+class ProjectMargin {
+  static const cardMarging = EdgeInsets.all(10);
+}
+
+class _CustomCards extends StatelessWidget {
+  //Custom Card yapıyorsak sadece Card içermeli Card dışındaki komponentler olmamalı
+  final Widget
+      child; //bu yüzden burada final key ile child tanımlayıp chil propunu customize edebiliriz
+
+  _CustomCards({Key? key, required this.child}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).colorScheme.error,
+      margin: ProjectMargin.cardMarging,
+      elevation: 100,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: child,
+    );
+  }
+}
+
+
+
+//StadiumBorder(), CircleBorder(), RoundedRectangleBorder()
